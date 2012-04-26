740389ee56c309107aea3766f7f789cf213525cb
Atit Patumvan <atit@patumvan.com>
2012-04-26 14:44:34 +0700

    Create MathUtil Class

---
+++ CViSign/MathUtil.h
@@ -0,0 +1,13 @@
+//
+//  MathUtil.h
+//  CViSign
+//
+//  Created by Atit Patumvan on 4/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MathUtil : NSObject
+
+@end
---
+++ CViSign/MathUtil.m
@@ -0,0 +1,13 @@
+//
+//  MathUtil.m
+//  CViSign
+//
+//  Created by Atit Patumvan on 4/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MathUtil.h"
+
+@implementation MathUtil
+
+@end
